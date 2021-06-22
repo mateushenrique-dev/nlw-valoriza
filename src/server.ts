e592865a779ef4673559e9a5357bc313208cc42a
Mateus Henrique <mhenriqq32@gmail.com>
2021-06-21 21:46:51 -0300

    Criando tabelas db

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import "./database";
 
 const app = express();
 
-app.get('/test', (request, response) => {
-  return response.send('Olá NLW!')
-})
-
-app.post('/test', (request, response) => {
-  return response.send('OLá NLW método post')
-})
-
-app.listen(3000, () => console.log('Server is Running.'))
-
+app.listen(3000);
